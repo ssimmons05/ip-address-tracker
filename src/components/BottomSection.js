b9bd8f9227d82c4fc47860f7e7c8b604e3ca6fa6
ssimmons05 <steve@snapit.solutions>
2021-06-23 23:38:52 -0500

    map function added

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
 
 const BottomSection = () => {
+    const position = [51.505, -0.09]
 
-    //  let mymap = L.map('map').setView([51.505, -0.09], 13);
-
-    //  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //     maxZoom: 18,
-    //     id: 'mapbox/streets-v11',
-    //     tileSize: 512,
-    //     zoomOffset: -1,
-    //     accessToken: 'pk.eyJ1Ijoic3RldmUwNSIsImEiOiJja3E4a3kwMmIwMjRpMm5vYnNjN2tlb3R0In0.gaND5Fwpv3rmauTmFraR2A'
-    //     }).addTo(mymap);
-
-    return (
-        <div id="map">
-       
-     
-        </div>
+    return(
+    
+    <div id = "map">
+      <MapContainer style={{height: "60vh", zIndex: "1", display: "absolute", top: "0"}}  center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
     )
 }
+    
 
 export default BottomSection
